34ad57d3dff2ea3bba995c8efff7868ca6a7a393
christina makatura <cnmak@comcast.net>
2022-01-15 19:39:29 -0600

    formatting homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,10 @@ import React from 'react'
 function Home(){
   return(
     <div className="homepage">
-    <img src="./images/elf.gif" alt="gift"/>
-    <h3>Happy holidays and welcome to The Gift Tracker, an app to keep track of all of your Christmas Shopping.</h3>
+    <img className="photo" src={'./images/wreath.png'} alt="gift"/>
+
+    <p classname="homepage">Happy holidays and welcome to The Gift Tracker,</p>
+    <p classname="homepage">an app to keep track of all of your Christmas Shopping.</p>
     </div>
   )
   
