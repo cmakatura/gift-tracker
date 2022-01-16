77bc54dde3ca636e4839f5a295790a16e5aba1fe
christina makatura <cnmak@comcast.net>
2022-01-16 13:00:47 -0600

    added filter for gifts

--- src/components/MyGifts.js
+++ src/components/MyGifts.js
@@ -2,7 +2,22 @@
 import React, {useState, useEffect} from 'react';
 
 
+
+
 function MyGifts() {
+
+
+  const [filter, setFilter] = useState("");
+
+
+
+
+
+
+
+
+
+
   const [data,setData]=useState([]);
   const getData=()=>{
     fetch("http://localhost:5000/gifts"
@@ -13,27 +28,56 @@ function MyGifts() {
        }
     }
     )
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
+      .then(response => response.json())
+      .then(json => setData(json))
   }
+
+
   useEffect(()=>{
     getData()
   },[])
+
+//console.log(filter)
+
+
+
+const results = data.filter((item) =>
+item.username.toLowerCase().includes(filter.toLowerCase())
+);
+
   return (
+<div>
+
+
+
+
+
+      <p className="filter" align="center">
+        Type to filter the list by your username: 
+        <input id="filter"
+          name="filter"
+          type="text"
+          value={filter}
+          onChange={event => setFilter(event.target.value)}
+        />
+      </p>
+      
+     
+
+
+
+
+
+
+
     <div className="gift-container" align="center">
      {
-       data.map((item)=>
+       results.map((item)=>
 
        <div className="gift" key={item.id}>
          
             <h3>For: {item.recipient}</h3>
-            
+        
             <h5>Gift: {item.gift}</h5>
             <h5>Price: ${item.price}</h5> 
             <h5>Hiding Spot: {item.hidingSpot}</h5> 
@@ -42,6 +86,7 @@ function MyGifts() {
        )
      }
     </div>
+  </div>
   );
 }
     
